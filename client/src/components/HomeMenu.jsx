ff75111eeb13f15999c27406d5efda7c45680b32
Bhavesh Patil <patilbhavesh781@gmail.com>
2025-12-29 22:11:01 +0530

    Started Building PGWala

---
+++ client/src/components/HomeMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeMenu = () => {
+  return (
+    <div>
+      This is HomeMenu!
+    </div>
+  )
+}
+
+export default HomeMenu
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import HomeMenu from '../components/HomeMenu'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <HomeMenu />
+      Hello, Teammates!
+    </div>
+  )
+}
+
+export default Home
