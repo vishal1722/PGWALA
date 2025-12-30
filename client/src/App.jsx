ee4db7b037aad63e79889c487ff7f5cc55226039
Chetan badgujar <badgujarchetan716@gmail.com>
2025-12-30 08:24:14 +0530

    dbhbh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='bg-gray-100 w-full h-full box-border'>
+    <div className="bg-gray-100 w-full h-full box-border">
       <Home />
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
